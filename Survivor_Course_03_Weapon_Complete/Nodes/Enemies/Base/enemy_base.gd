5034f3263477b038f6d4bcf8e93abf4666392d75
Tomasz Szymański <cl.arctos.orion@gmail.com>
2023-12-19 15:22:11 +0100

    Add Course 03 Weapon project

---
+++ Survivor_Course_03_Weapon_Complete/Nodes/Enemies/Base/enemy_base.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+class_name EnemyBase
+
+@export var moveSpeed: float = 100.0
+var moveDirection: Vector2 = Vector2.ZERO
+
+@export var targetNode: Node2D
+
+@onready var moveSpeedCurrent: float = moveSpeed
+
+func _process(delta: float) -> void:
+	_move(targetNode, delta)
+	_lookAtTarget(targetNode)
+
+# Movement
+
+func _move(inTarget: Node2D, _inDelta: float) -> void:
+	if not is_instance_valid(inTarget):
+		return
+	
+	moveDirection = _getTargetVector(targetNode)
+	
+	velocity = moveDirection.normalized() * moveSpeedCurrent
+	move_and_slide()
+
+
+func _lookAtTarget(inTarget: Node2D) -> void:
+	if not is_instance_valid(inTarget):
+		return
+	
+	look_at(inTarget.global_position)
+
+# Getters
+
+func _getTargetVector(inTarget: Node2D) -> Vector2:
+	if not is_instance_valid(inTarget):
+		return Vector2.ZERO
+	
+	var result: Vector2 = Vector2.ZERO
+	
+	result = inTarget.global_position - self.global_position
+	
+	return result
